2e3fd9e98453479fd82732bc1793dd747193d192
max-borisov <max.borisov@yahoo.com>
2015-03-19 17:58:30 +0200

    Update app config. Add new params.

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,4 +1,7 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'shortArticleLength' => 220,
+    'articlePreviewPlaceholder' => 'http://placehold.it/200x52',
+    'linkPagerButtonsCount' => 15,
 ];
