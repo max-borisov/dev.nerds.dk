265b6640356e14a2b4b021b89c68f554d3acd091
max-borisov <max.borisov@yahoo.com>
2015-03-23 22:15:02 +0200

    Add parser models.

---
+++ frontend/models/AdType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use frontend\models\ActiveRecord;
+
+/**
+ * This is the model class for table "ad_type".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class AdType extends ActiveRecord
+{
+    const SELL      = 1;
+    const BUY       = 2;
+    const EXCHANGE  = 3;
+    const UNKNOWN   = 4;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ad_type';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 100],
+            [['title'], 'unique']
+        ];
+    }
+
+    /**
+     * Retrieve data (id and title)
+     * @return array
+     */
+    public function prepareList()
+    {
+        $data = (new \yii\db\Query())
+            ->select('id, title')
+            ->from($this->tableName())
+            ->where('id < 4')
+            ->orderBy('id ASC')
+            ->all();
+        return ArrayHelper::map($data, 'id', 'title');
+    }
+
+    /**
+     * Build relation with Item model
+     * @return ActiveQuery
+     */
+    public function getItems()
+    {
+        return $this->hasMany(Item::className(), ['ad_type_id' => 'id'])->orderBy('created_at ASC');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
---
+++ frontend/models/Item.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use frontend\models\Category;
+use frontend\models\ItemPhoto;
+use frontend\components\HelperBase;
+//use frontend\components\HelperMarketPlace;
+use frontend\components\HelperUser;
+use yii\base\Exception;
+
+/**
+ * This is the model class for table "item".
+ *
+ * @property integer $id
+ * @property integer $warranty
+ * @property integer $invoice
+ * @property integer $packaging
+ * @property integer $manual
+ * @property string $price
+ * @property integer $category_id
+ * @property string $title
+ * @property integer $user_id
+ * @property integer $ad_type_id
+ * @property string $description
+ * @property string $preview
+ * @property string $price_min
+ * @property string $price_max
+ * @property integer $site_id
+ * @property string $s_item_id
+ * @property string $s_user
+ * @property string $s_location
+ * @property string $s_phone
+ * @property string $s_email
+ * @property string $s_type
+ * @property string $s_adv
+ * @property string $s_date
+ * @property string $s_preview
+ * @property string $s_age
+ * @property string $s_warranty
+ * @property string $s_package
+ * @property string $s_delivery
+ * @property string $s_akn
+ * @property string $s_manual
+ * @property string $s_expires
+ * @property string $s_brand
+ * @property string $s_model
+ * @property string $s_producer
+ * @property string $s_watt
+ * @property string $s_product
+ * @property string $media_title
+ * @property string $media_genre
+ * @property string $media_type
+ * @property string $media_producer
+ * @property string $music_artist
+ * @property string $media_features
+ * @property string $media_inches
+ * @property string $media_size
+ * @property string $eq_capacity
+ * @property string $hd_capacity
+ * @property string $camera_resolution
+ * @property string $optical_zoom
+ * @property string $speaker
+ * @property string $speaker_type
+ * @property string $channels
+ */
+class Item extends \app\components\ActiveRecord
+{
+    const YES_FLAG  = 1;
+    const NO_FLAG   = 0;
+    const NA_FLAG   = 2;
+
+    public $price_min;
+    public $price_max;
+    public $preview;
+    public $search_text;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'item';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+//            [['warranty', 'invoice', 'packaging', 'manual', 'price', 'category_id', 'title', 'ad_type_id', 'description'], 'required', 'on' => ['create', 'edit']],
+            [['title', 'category_id', 'ad_type_id', 'description'], 'required', 'on' => ['create', 'edit']],
+            [['warranty', 'invoice', 'packaging', 'manual', 'category_id', 'ad_type_id'], 'integer', 'on' => ['create', 'edit']],
+            [['price'], 'number', 'on' => ['create', 'edit']],
+            [['price'], 'default', 'value' => 0, 'on' => ['create', 'edit']],
+            [['title'], 'string', 'max' => 255, 'on' => ['create', 'edit']],
+            [['description'], 'string', 'on' => ['create', 'edit']],
+
+            [['title', 'description'], 'filter', 'filter' => function ($value) {
+                    return trim(strip_tags($value));
+            }, 'on' => ['create', 'edit']],
+
+            [['warranty', 'packaging', 'manual', 'ad_type_id'], 'integer', 'on' => ['search']],
+            [['search_text'], 'string', 'max' => 255, 'on' => ['search']],
+            [['price_min, price_max'], 'number', 'on' => ['search']],
+        ];
+    }
+
+    /**
+     * Build relation with Category model
+     * @return ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+
+    /**
+     * Build relation with ItemPhoto model
+     * @return ActiveQuery
+     */
+    public function getPhotos()
+    {
+        return $this->hasMany(ItemPhoto::className(), ['item_id' => 'id'])->orderBy('updated_at DESC');
+    }
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    public function getType()
+    {
+        return $this->hasOne(AdType::className(), ['id' => 'ad_type_id']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'warranty' => 'Warranty:',
+            'invoice' => 'Invoice:',
+            'packaging' => 'Packaging:',
+            'manual' => 'Manual:',
+            'price' => 'Price:',
+            'category_id' => 'Category:',
+            'title' => 'Title:',
+            'search_text' => 'Search text:',
+            'user_id' => 'User id',
+            'ad_type_id' => 'Ad type:',
+            'description' => 'Description:',
+            'price_min' => 'Min price:',
+            'price_max' => 'Max price:',
+            'created_at' => 'Created:',
+
+            // Parsed data
+            's_user' => 'User:',
+            's_location' => 'Location:',
+            's_phone' => 'Phone:',
+            's_email' => 'Email:',
+            's_adv' => 'Advertisement:',
+            's_age' => 'Age:',
+            's_date' => 'Post date:',
+            's_warranty' => 'Warranty:',
+            's_package' => 'Package:',
+            's_delivery' => 'Delivery:',
+            's_manual' => 'Manual:',
+            's_akn' => 'Receipt:',
+            's_expires' => 'Expires:',
+
+            's_brand'       => 'Brand:',
+            's_model'       => 'Model:',
+            's_producer'    => 'Producer:',
+            's_watt'        => 'Watt:',
+            's_product'     => 'Product:',
+
+            'media_title'       => 'Media title:',
+            'media_genre'       => 'Genre:',
+            'media_type'        => 'Type:',
+            'media_producer'    => 'Producer:',
+
+            'music_artist'      => 'Artist:',
+            'media_features'    => 'Features:',
+            'media_inches'      => 'Inches:',
+            'media_size'        => 'Size:',
+            'eq_capacity'       => 'Capacity(GB):',
+            'hd_capacity'       => 'HD Capacity(GB):',
+            'camera_resolution' => 'Resolution:',
+            'optical_zoom'      => 'Optical zoom:',
+            'speaker'           => 'Speaker:',
+            'speaker_type'      => 'Speaker type:',
+            'channels'          => 'Channels:',
+        ];
+    }
+
+    /**
+     * Apply search filter for items
+     * @param array $params Get parameters (form data)
+     * @return array|bool|\yii\db\ActiveRecord[]
+     */
+    public function search($params)
+    {
+        if (!($this->load($params) || !$this->validate())) {
+            return false;
+        }
+
+        $this->price_min = Yii::$app->request->get('Item')['price_min'];
+        $this->price_max = Yii::$app->request->get('Item')['price_max'];
+        $price_min = (int)$this->price_min;
+        $price_max = (int)$this->price_max;
+
+        $query = Item::find();
+        $query->where('category_id > 0');
+        $ad_id = (int)$this->ad_type_id;
+        if ($ad_id > 0) {
+            $query->andWhere('ad_type_id = :ad_id', [':ad_id' => $ad_id]);
+        }
+        if (isset($this->warranty)) {
+            $query->andWhere('warranty = :warranty OR warranty = :na_flag', [':warranty' => (int)$this->warranty, ':na_flag' => Item::NA_FLAG]);
+        }
+        if (isset($this->packaging)) {
+            $query->andWhere('packaging = :packaging OR packaging = :na_flag', [':packaging' => (int)$this->packaging, ':na_flag' => Item::NA_FLAG]);
+        }
+        if (isset($this->manual)) {
+            $query->andWhere('manual = :manual OR manual = :na_flag', [':manual' => (int)$this->manual, ':na_flag' => Item::NA_FLAG]);
+        }
+        if ($price_min > 0) {
+            $query->andWhere('price >= :price_min', [':price_min' => $price_min]);
+        }
+        if ($price_max > 0) {
+            $query->andWhere('price <= :price_max', [':price_max' => $price_max]);
+        }
+        if ($this->search_text) {
+            $query->andWhere(['or', ['like', 'title', $this->search_text], ['like', 'description', $this->search_text]]);
+        }
+        $query->orderBy(HelperMarketPlace::getSortParamForItemsList());
+        return $query;
+    }
+
+    public function beforeSave($insert)
+    {
+        if (!isset($this->user_id)) {
+            throw new Exception('User id cannot be blank.');
+        }
+        return parent::beforeSave($insert);
+    }
+
+    public function afterFind()
+    {
+        // Set preview for each item
+        // Default(blank) preview
+        $this->preview = HelperBase::getParam('thumb')['placeholder'];
+
+        if (!empty($this->s_preview) && $this->site_id == ExternalSite::HIFI4ALL) {
+            $this->preview = HelperBase::getParam('HiFi4AllPic') . '/' . $this->s_preview;
+        }
+        if (!empty($this->s_preview) && $this->site_id == ExternalSite::DBA) {
+            $this->preview = $this->s_preview;
+        }
+
+        if (($photos = $this->photos) && is_array($photos)) {
+            $photoName = $photos[0]->name;
+            $photoPath =
+                Yii::getAlias('@photo_thumb_path')
+                . '/'
+                . $photoName;
+            if (file_exists($photoPath)) {
+                $this->preview =
+                    Yii::getAlias('@photo_thumb_url')
+                    . '/'
+                    . $photoName;
+            }
+        }
+        parent::afterFind();
+    }
+
+    public function beforeDelete()
+    {
+        // Get related photo models and delete them
+        foreach ($this->photos as $photoModel) {
+            if (!$photoModel->delete()) {
+                throw new Exception('Photo model with id ' . $photoModel->id . ' could not be deleted.');
+            }
+        }
+
+        return parent::beforeDelete();
+    }
+}
