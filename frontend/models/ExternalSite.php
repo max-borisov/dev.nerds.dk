81cbd44460f6050433fb9522dbf09b6d91bd30ca
max-borisov <max.borisov@yahoo.com>
2015-03-23 21:52:26 +0200

    Add Site model.

---
+++ frontend/models/ExternalSite.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "site".
+ *
+ * @property integer $id
+ * @property string $title
+ */
+class ExternalSite extends \yii\db\ActiveRecord
+{
+    const HIFI4ALL  = 1;
+    const RECORDERE = 2;
+    const DBA       = 3;
+    const NERDS     = 4;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'site';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+}
