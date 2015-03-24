cdf23f2f6942ff34c0e4cec1baaa7ee3cce6f64e
max-borisov <max.borisov@yahoo.com>
2015-03-24 09:46:27 +0200

    Update stat page

--- frontend/controllers/StatController.php
+++ frontend/controllers/StatController.php
@@ -2,19 +2,17 @@
 
 namespace frontend\controllers;
 
-use frontend\components\HelperBase;
-
 use Yii;
 use yii\helpers;
 use frontend\models\Item;
 use frontend\models\News;
 use frontend\models\Review;
-use frontend\models\Game;
-use frontend\models\Tv;
-use frontend\models\Music;
-use frontend\models\Movie;
-use frontend\models\Media;
-use frontend\models\Radio;
+use frontend\models\ParserGame;
+use frontend\models\ParserTv;
+use frontend\models\ParserMusic;
+use frontend\models\ParserMovie;
+use frontend\models\ParserMedia;
+use frontend\models\ParserRadio;
 use frontend\models\ExternalSite;
 
 class StatController extends AppController
@@ -28,12 +26,12 @@ class StatController extends AppController
         $reviewsHiFi    = Review::find()->where('site_id = ' . ExternalSite::HIFI4ALL)->count();
         $reviewsRec     = Review::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
 
-        $gamesRec   = Game::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
-        $tvRec      = Tv::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
-        $musicRec   = Music::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
-        $moviesRec  = Movie::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
-        $mediaRec   = Media::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
-        $radioRec   = Radio::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
+        $gamesRec   = ParserGame::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
+        $tvRec      = ParserTv::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
+        $musicRec   = ParserMusic::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
+        $moviesRec  = ParserMovie::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
+        $mediaRec   = ParserMedia::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
+        $radioRec   = ParserRadio::find()->where('site_id = ' . ExternalSite::RECORDERE)->count();
 
         $itemsTotal     = $itemsHiFi + $itemsDba;
         $newsTotal      = $newsHiFi + $newsRec;
