c120611f27b64c1dfb5aac035f035e3ac572d405
max-borisov <max.borisov@yahoo.com>
2015-03-26 16:39:31 +0200

    Delete old tables.

---
+++ console/migrations/m150326_142724_delete_tables.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150326_142724_delete_tables extends Migration
+{
+    public function safeUp()
+    {
+        $this->dropTable('item_catalog');
+        $this->dropTable('item_dba');
+    }
+    
+    public function safeDown()
+    {
+        echo 'This action is irreversible.';
+    }
+}
