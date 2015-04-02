ba6107ab48fd255b230bd7abd3c3d2882bee6ba8
max-borisov <max.borisov@yahoo.com>
2015-04-02 12:16:04 +0300

    Update app config.

--- frontend/views/shared/header.php
+++ frontend/views/shared/header.php
@@ -1,6 +1,16 @@
+<?php
+use yii\helpers\Url;
+use frontend\components\HelperBase;
+
+$siteUrl = HelperBase::getParam('siteUrl');
+$siteName = str_replace('http://', '', $siteUrl);
+?>
 <header id="header">
     <div class="header-top">
-        <h1 class="logo"><a href="http://nerds.dk/"><img alt="Nerds.dk" src="/images/logo.jpg"></a></h1>
+        <h1 class="logo">
+            <a href="<?= Url::to(HelperBase::getParam('siteUrl')) ?>">
+                <img alt="<?= $siteName ?>" src="/images/logo.jpg">
+            </a></h1>
         <div class="header-top-banner">
             <img alt="header banner 728*90" src="/images/banner_728-90.jpg">
         </div>
