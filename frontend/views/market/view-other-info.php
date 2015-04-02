a0f3bf6ddc907cedc89092dcb797abeea766156e
max-borisov <max.borisov@yahoo.com>
2015-04-02 15:13:43 +0300

    Update item view page.

--- frontend/views/market/view-other-info.php
+++ frontend/views/market/view-other-info.php
@@ -26,39 +26,167 @@ if ($item->s_invoice) {
 }
 ?>
 <h2>Øvrige oplysninger</h2>
-<div class="table-responsive margin-down">
-    <table class="table table-hover table-striped marketplace-table">
-        <tbody>
-        <tr>
-            <td><strong>Garanti:</strong></td>
-            <td><?= $warranty ?></td>
-            <td><strong>Kvittering:</strong></td>
-            <td><?= $invoice ?></td>
-            <td><strong>Emballage:</strong></td>
-            <td><?= $package ?></td>
-            <td><strong>Manual:</strong></td>
-            <td>Ja</td>
-        </tr>
-        <tr>
-            <td><strong>Antal visninger:</strong></td>
-            <td>0</td>
-            <td><strong>Antal mails:</strong></td>
-            <td>0</td>
-            <td><strong>Age:</strong></td>
-            <td><?= $item->s_age ?></td>
-            <td><strong>Delivery:</strong></td>
-            <td><?= $item->s_delivery ?></td>
-        </tr>
-        <tr>
-            <td><strong>Brand:</strong></td>
-            <td><?= $item->s_brand ?></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        </tbody>
-    </table>
-</div>
+<ul class="item-def">
+    <li>
+        <strong>Garanti:</strong>
+        <span><?= $warranty ?></span>
+    </li>
+    <li>
+        <strong>Kvittering:</strong>
+        <span><?= $invoice ?></span>
+    </li>
+    <li>
+        <strong>Emballage:</strong>
+        <span><?= $package ?></span>
+    </li>
+    <li>
+        <strong>Manual:</strong>
+        <span><?= $manual ?></span>
+    </li>
+    <li>
+        <strong>Antal visninger:</strong>
+        <span>0</span>
+    </li>
+    <li>
+        <strong>Antal mailsr:</strong>
+        <span>0</span>
+    </li>
+    <?php if ($item->s_age) { ?>
+        <li>
+            <strong>Age:</strong>
+            <span><?= $item->s_age ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_delivery) { ?>
+        <li>
+            <strong>Delivery:</strong>
+            <span><?= $item->s_delivery ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_brand) { ?>
+        <li>
+            <strong>Brand:</strong>
+            <span><?= $item->s_brand ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_expires) { ?>
+        <li>
+            <strong>Expires:</strong>
+            <span><?= $item->s_expires ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_model) { ?>
+        <li>
+            <strong>Model:</strong>
+            <span><?= $item->s_model ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_producer) { ?>
+        <li>
+            <strong>Producer:</strong>
+            <span><?= $item->s_producer ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_watt) { ?>
+        <li>
+            <strong>Watt:</strong>
+            <span><?= $item->s_watt ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->s_product) { ?>
+        <li>
+            <strong>Product:</strong>
+            <span><?= $item->s_product ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_title) { ?>
+        <li>
+            <strong>Title:</strong>
+            <span><?= $item->media_title ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_genre) { ?>
+        <li>
+            <strong>Genre:</strong>
+            <span><?= $item->media_genre ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_type) { ?>
+        <li>
+            <strong>Type:</strong>
+            <span><?= $item->media_type ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_producer) { ?>
+        <li>
+            <strong>Producer:</strong>
+            <span><?= $item->media_producer ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->music_artist) { ?>
+        <li>
+            <strong>Artist:</strong>
+            <span><?= $item->music_artist ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_features) { ?>
+        <li>
+            <strong>Features:</strong>
+            <span><?= $item->media_features ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_inches) { ?>
+        <li>
+            <strong>Inches:</strong>
+            <span><?= $item->media_inches ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->media_size) { ?>
+        <li>
+            <strong>Size:</strong>
+            <span><?= $item->media_size ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->eq_capacity) { ?>
+        <li>
+            <strong>Capacity:</strong>
+            <span><?= $item->eq_capacity ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->hd_capacity) { ?>
+        <li>
+            <strong>HDD Capacity:</strong>
+            <span><?= $item->hd_capacity ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->camera_resolution) { ?>
+        <li>
+            <strong>Resolution:</strong>
+            <span><?= $item->camera_resolution ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->optical_zoom) { ?>
+        <li>
+            <strong>Optical zoom:</strong>
+            <span><?= $item->optical_zoom ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->speaker) { ?>
+        <li>
+            <strong>Speaker:</strong>
+            <span><?= $item->speaker ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->speaker_type) { ?>
+        <li>
+            <strong>Speaker type:</strong>
+            <span><?= $item->speaker_type ?></span>
+        </li>
+    <?php } ?>
+    <?php if ($item->channels) { ?>
+        <li>
+            <strong>Channels:</strong>
+            <span><?= $item->channels ?></span>
+        </li>
+    <?php } ?>
+</ul>
