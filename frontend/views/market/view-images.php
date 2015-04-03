4b9795787b8ef3aa9e83580210d9f69b8f6d4995
max-borisov <max.borisov@yahoo.com>
2015-04-03 13:47:32 +0300

    Update item page. Show image description.

--- frontend/views/market/view-images.php
+++ frontend/views/market/view-images.php
@@ -5,7 +5,7 @@
 <?php if (!empty($item->preview)) { ?>
     <div class="row img-row margin-bottom">
         <div class="col-sm-3">
-            <a href="<?= $item->preview ?>" data-toggle="lightbox" data-gallery="global-gallery" data-parent=".img-row" data-footer="Item description">
+            <a href="<?= $item->preview ?>" data-toggle="lightbox" data-gallery="global-gallery" data-parent=".img-row" data-footer="<?= $item->description ?>">
                 <img alt="product description" src="<?= $item->preview ?>" data-src="<?= $item->preview ?>" class="img-responsive">
             </a>
         </div>
