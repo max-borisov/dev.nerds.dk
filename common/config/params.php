5e15438af0134d909461299e2424d44227e6d631
max-borisov <max.borisov@yahoo.com>
2015-04-08 12:05:35 +0300

    Update add config.

--- common/config/params.php
+++ common/config/params.php
@@ -3,7 +3,8 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-
+    // Items images base url
+    'HiFi4AllPic' => 'http://www.hifi4all.dk/ksb',
     'images' => [
         'pathToOriginal'  => '@webroot/images/original/',
         'pathToThumb'     => '@webroot/images/thumb/',
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,6 +3,4 @@ return [
     'adminEmail' => 'admin@example.com',
     'shortArticleLength' => 220,
     'linkPagerButtonsCount' => 15,
-    // Items images base url
-    'HiFi4AllPic' => 'http://www.hifi4all.dk/ksb',
 ];
