25b4441df7bf302c80e284722fc0121664a07522
max-borisov <max.borisov@yahoo.com>
2015-04-10 18:07:26 +0300

    Update HiFiItems parser.

--- console/components/Parser/HiFi4All/HiFiItems.php
+++ console/components/Parser/HiFi4All/HiFiItems.php
@@ -8,12 +8,14 @@ use frontend\models\Category;
 use frontend\models\ExternalSite;
 use frontend\models\AdType;
 use frontend\models\Item;
+use common\models\User;
 
 require_once __DIR__ . '/../Base.php';
 
 class HiFiItems extends Base
 {
     private $_baseUrl = 'http://www.hifi4all.dk/ksb/';
+    private $_pageId;
 
     /**
      * Parse page by unique id. Get all necessary info.
@@ -25,139 +27,37 @@ class HiFiItems extends Base
     {
         $html = $this->tidy($this->_baseUrl . 'Annonce.asp?id=' . $id);
         $data = [];
-        $data['id'] = $id;
+        $data['id'] = $this->_pageId = $id;
 
-        $pattern = '|<td\s+width="604"\s+valign="top">(.*)</td>|is';
-        preg_match_all($pattern, $html, $matches);
-        $root = $matches[0][0];
-        $root = str_replace('</font>', '', $root);
-        $root = preg_replace('|<font[^>]+>|is', '', $root);
-        $root = str_replace(['<br>', '<br/>', '<br />'], '', $root);
-        $root = $this->iconv($root);
+        // Root html
+        $root = $this->_getRootHtml($html);
 
         // Item title
-        $pattern = '|<td\s+width="90%"\s+background=".*?">\s+<b>(.*?)</b>\s+</td>\s+|isx';
-        preg_match_all($pattern, $root, $matches);
-        if (isset($matches[1], $matches[1][0])) {
-            $data['title'] = trim($matches[1][0]);
-            $data['title'] = substr($data['title'], 0, strrpos($data['title'], '-')-1);
-        } else {
-            throw new Exception('Could not get item title. Page id ' . $id);
-        }
+        $data['title'] = $this->_getTitle($root);
 
         // User name
-        $pattern = '|<td\s+width="37%">([^<]+)<a href=(?:.*?)></a>\s+</td>|is';
-        preg_match_all($pattern, $root, $matches);
-        if (isset($matches[1], $matches[1][0])) {
-            $data['user'] = trim($matches[1][0]);
-        } else {
-            throw new Exception('Could not get user name. Page id ' . $id);
-        }
+        $data['user'] = $this->_getName($root);
 
         // Location and phone
-        $pattern = '|<td\s+width="37%">(.*?)</td>|is';
-        preg_match_all($pattern, $root, $matches);
-        if (isset($matches[1], $matches[1][1])) {
-            $data['location'] = trim($matches[1][1]);
-        } else {
-            throw new Exception('Could not get user location. Page id ' . $id);
-        }
-        if (isset($matches[1], $matches[1][2])) {
-            $phone = trim($matches[1][2]);
-            if (preg_match('/[a-z]+/', $phone)) {
-                $phone = '';
-            }
-            $data['phone'] = $phone;
-        } else {
-            throw new Exception('Could not get user phone. Page id ' . $id);
-        }
+        list($data['location'], $data['phone']) = array_values($this->_getLocationAndPhone($root));
 
         // Email
-        if (isset($matches[1], $matches[1][3])) {
-            $emailRaw = $matches[1][3];
-            if (strpos($emailRaw, 'Privat') !== false) {
-                $email = '';
-            } else {
-                $emailRaw = strip_tags($emailRaw, '<img>');
-                $emailRaw = str_replace('//', '', $emailRaw);
-                $pattern = '|[^<]+|is';
-                preg_match_all($pattern, $emailRaw, $matches);
-                $emailPartName = $matches[0][0];
-                $pattern = '|>([\w./]+)|is';
-                preg_match_all($pattern, $emailRaw, $matches);
-                $emailPartDomain = $matches[0][0];
-                $emailPartDomain = str_replace('>', '', $emailPartDomain);
-                $email = trim($emailPartName . '@' . $emailPartDomain);
-            }
-            $data['email'] = $email;
-        } else {
-            throw new Exception('Could not get user email. Page id ' . $id);
-        }
+        $data['email'] = $this->_getEmail($root);
 
         // Item type, Ad type, pub date
-        $pattern = '|<td\s+width="28%">(.*?)</td>|is';
-        preg_match_all($pattern, $root, $matches);
-        if (isset($matches[1], $matches[1][0])) {
-            $data['type'] = trim($matches[1][0]);
-        } else {
-            throw new Exception('Could not get item type. Page id ' . $id);
-        }
-        if (isset($matches[1], $matches[1][1])) {
-            $data['adv'] = trim($matches[1][1]);
-        } else {
-            throw new Exception('Could not get advertisement type. Page id ' . $id);
-        }
-        if (isset($matches[1], $matches[1][2])) {
-            $data['date'] = trim($matches[1][2]);
-        } else {
-            throw new Exception('Could not get pub date. Page id ' . $id);
-        }
+        list($data['type'], $data['adv'], $data['date']) = array_values($this->_getTypeAndAdvAndDate($root));
 
         // Price
-        $pattern = '|<b>[\w./\s]*pris:\s+(\d+)(?:&nbsp;)?DKK</b>|is';
-        preg_match_all($pattern, $root, $matches);
-        if (empty($matches[0])) {
-            $price = 0;
-        } else {
-            $price = $matches[1][0];
-        }
-        $data['price'] = $price;
+        $data['price'] = $this->_getPrice($root);
 
         // Description
-//        $pattern = '|<td\s+width="100%"\s+valign="top">([^<]+)</td>|is';
-        $pattern = '|<td\s+width="100%"\s+valign="top">(.*?)</td>|is';
-        preg_match_all($pattern, $root, $matches);
-        if (isset($matches[1], $matches[1][2])) {
-            $description = trim($matches[1][2]);
-            $description = preg_replace('/\s+/', ' ', $description);
-        } else {
-            $description = '';
-        }
-        $data['description'] = $description;
+        $data['description'] = $this->_getDescription($root);
 
         // Preview
-        $pattern = '|src="(pics/.*?)"\s+alt=|is';
-        preg_match_all($pattern, $root, $matches);
-        if (empty($matches[0])) {
-            $preview = '';
-        } else {
-            $preview = $matches[1][0];
-        }
-        $data['preview'] = $preview;
+        $data['preview'] = $this->_getPreview($root);
 
         // Additional info
-        $pattern = '|<td\s+height="16">(.*?)</td>|is';
-        preg_match_all($pattern, $root, $matches);
-        $addInfo = [
-            'age'       => trim($matches[1][0]),
-            'warranty'  => trim($matches[1][1]),
-            'package'   => trim($matches[1][2]),
-            'delivery'  => trim($matches[1][3]),
-            'ack'       => trim($matches[1][4]),
-            'manual'    => trim($matches[1][5]),
-            'expires'   => trim($matches[1][6]),
-        ];
-        $data['info'] = $addInfo;
+        $data['info'] = $this->_getAdditionalInfo($root);
 
         return $data;
     }
@@ -171,7 +71,6 @@ class HiFiItems extends Base
     public function getCatalogLinks($offset)
     {
         $html = $this->tidy($this->_baseUrl . 'index.asp?offset=' . $offset);
-        $data = [];
         $pattern = '|<td\s+width="224">(?:.*?)(\d+)(?:.*?)</td>|is';
         preg_match_all($pattern, $html, $matches);
         if (empty($matches[1])) {
@@ -190,7 +89,7 @@ class HiFiItems extends Base
     public function saveItem($data)
     {
         $item = new Item();
-        $item->user_id      = 112233;
+        $item->user_id      = User::UNKNOWN;
         $item->category_id  = Category::HIFI4ALL;
         $item->ad_type_id   = AdType::UNKNOWN;
 
@@ -245,6 +144,11 @@ class HiFiItems extends Base
 
         if (!$item->save(false)) {
             throw new Exception('Data could not be saved. Id ' . $data['site_id']);
+        }
+        if ($item->s_preview) {
+            $path = Yii::$app->image->copy($item->s_preview, $item->created_at)->path();
+            $item->preview = substr($path, strrpos($path, '/')+1);
+            $item->save(false);
         }
         return $item->id;
     }
@@ -304,5 +208,164 @@ class HiFiItems extends Base
         }
         $after = $this->getExistingRowsCount('item', ExternalSite::HIFI4ALL);
         $this->done('HiFiItems', $before, $after);
+    }
+
+    private function _getRootHtml($html)
+    {
+        $pattern = '|<td\s+width="604"\s+valign="top">(.*)</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        $root = $matches[0][0];
+        $root = str_replace('</font>', '', $root);
+        $root = preg_replace('|<font[^>]+>|is', '', $root);
+        $root = str_replace(['<br>', '<br/>', '<br />'], '', $root);
+        return $this->iconv($root);
+    }
+
+    private function _getTitle($html)
+    {
+        $pattern = '|<td\s+width="90%"\s+background=".*?">\s+<b>(.*?)</b>\s+</td>\s+|isx';
+        preg_match_all($pattern, $html, $matches);
+        if (isset($matches[1], $matches[1][0])) {
+            $title = trim($matches[1][0]);
+            return substr($title, 0, strrpos($title, '-')-1);
+        } else {
+            throw new Exception('Could not get item title. Page id ' . $this->_pageId);
+        }
+    }
+
+    private function _getName($html)
+    {
+        $pattern = '|<td\s+width="37%">([^<]+)<a href=(?:.*?)></a>\s+</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        if (isset($matches[1], $matches[1][0])) {
+            return trim($matches[1][0]);
+        } else {
+            throw new Exception('Could not get user name. Page id ' . $this->_pageId);
+        }
+    }
+
+    private function _getLocationAndPhone($html)
+    {
+        $data = [];
+        $pattern = '|<td\s+width="37%">(.*?)</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        if (isset($matches[1], $matches[1][1])) {
+            $data['location'] = trim($matches[1][1]);
+        } else {
+            throw new Exception('Could not get user location. Page id ' . $this->_pageId);
+        }
+        if (isset($matches[1], $matches[1][2])) {
+            $phone = trim($matches[1][2]);
+            if (preg_match('/[a-z]+/', $phone)) {
+                $phone = '';
+            }
+            $data['phone'] = $phone;
+        } else {
+            throw new Exception('Could not get user phone. Page id ' . $this->_pageId);
+        }
+        return $data;
+    }
+
+    private function _getEmail($html)
+    {
+        $pattern = '|<td\s+width="37%">(.*?)</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        if (isset($matches[1], $matches[1][3])) {
+            $emailRaw = $matches[1][3];
+            if (strpos($emailRaw, 'Privat') !== false) {
+                $email = '';
+            } else {
+                $emailRaw = strip_tags($emailRaw, '<img>');
+                $emailRaw = str_replace('//', '', $emailRaw);
+                $pattern = '|[^<]+|is';
+                preg_match_all($pattern, $emailRaw, $matches);
+                $emailPartName = $matches[0][0];
+                $pattern = '|>([\w./]+)|is';
+                preg_match_all($pattern, $emailRaw, $matches);
+                $emailPartDomain = $matches[0][0];
+                $emailPartDomain = str_replace('>', '', $emailPartDomain);
+                $email = trim($emailPartName . '@' . $emailPartDomain);
+            }
+            return $email;
+        } else {
+            throw new Exception('Could not get user email. Page id ' . $this->_pageId);
+        }
+    }
+
+    private function _getTypeAndAdvAndDate($html)
+    {
+        $data = [];
+        $pattern = '|<td\s+width="28%">(.*?)</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        if (isset($matches[1], $matches[1][0])) {
+            $data['type'] = trim($matches[1][0]);
+        } else {
+            throw new Exception('Could not get item type. Page id ' . $this->_pageId);
+        }
+        if (isset($matches[1], $matches[1][1])) {
+            $data['adv'] = trim($matches[1][1]);
+        } else {
+            throw new Exception('Could not get advertisement type. Page id ' . $this->_pageId);
+        }
+        if (isset($matches[1], $matches[1][2])) {
+            $data['date'] = trim($matches[1][2]);
+        } else {
+            throw new Exception('Could not get pub date. Page id ' . $this->_pageId);
+        }
+        return $data;
+    }
+
+    private function _getPrice($html)
+    {
+        $pattern = '|<b>[\w./\s]*pris:\s+(\d+)(?:&nbsp;)?DKK</b>|is';
+        preg_match_all($pattern, $html, $matches);
+        if (empty($matches[0])) {
+            $price = 0;
+        } else {
+            $price = $matches[1][0];
+        }
+        return $price;
+    }
+
+    private function _getDescription($html)
+    {
+//        $pattern = '|<td\s+width="100%"\s+valign="top">([^<]+)</td>|is';
+        $pattern = '|<td\s+width="100%"\s+valign="top">(.*?)</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        if (isset($matches[1], $matches[1][2])) {
+            $description = trim($matches[1][2]);
+            $description = preg_replace('/\s+/', ' ', $description);
+        } else {
+            $description = '';
+        }
+
+        return $description;
+    }
+
+    private function _getPreview($html)
+    {
+        $pattern = '|src="(pics/.*?)"\s+alt=|is';
+        preg_match_all($pattern, $html, $matches);
+        if (empty($matches[0])) {
+            $preview = '';
+        } else {
+            $preview = $this->_baseUrl . $matches[1][0];
+        }
+        return $preview;
+    }
+
+    private function _getAdditionalInfo($html)
+    {
+        $pattern = '|<td\s+height="16">(.*?)</td>|is';
+        preg_match_all($pattern, $html, $matches);
+        return [
+            'age'       => trim($matches[1][0]),
+            'warranty'  => trim($matches[1][1]),
+            'package'   => trim($matches[1][2]),
+            'delivery'  => trim($matches[1][3]),
+            'ack'       => trim($matches[1][4]),
+            'manual'    => trim($matches[1][5]),
+            'expires'   => trim($matches[1][6]),
+        ];
     }
 }
