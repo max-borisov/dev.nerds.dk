0459f7f4130e7d5a6ebdf05e145b805983dd5692
max-borisov <max.borisov@yahoo.com>
2015-04-10 18:07:49 +0300

    Update Sandbox controller.

--- frontend/controllers/SandboxController.php
+++ frontend/controllers/SandboxController.php
@@ -23,21 +23,28 @@ class SandboxController extends Controller
 
     public function actionParser()
     {
-//        require_once Yii::getAlias('@console') . '/components/Parser/HiFi4All/HiFiItems.php';
-//        (new HiFiItems())->run();
+        require_once Yii::getAlias('@console') . '/components/Parser/HiFi4All/HiFiItems.php';
+        (new HiFiItems())->run();
+//        $testId = '289792';
+//        $data = (new HiFiItems())->parsePage($testId);
+//        HelperBase::dump($data);
+//        (new HiFiItems())->saveItem($data);
 
-        require_once Yii::getAlias('@console') . '/components/Parser/Dba/DbaItems.php';
-        (new DbaItems())->run();
+        /*require_once Yii::getAlias('@console') . '/components/Parser/Dba/DbaItems.php';
+        (new DbaItems())->run();*/
     }
 
     public function actionImages()
     {
-        $res = Yii::$app->image->original('055239df7c3924.jpg')->path();
+//        $res = Yii::$app->image->original('055239df7c3924.jpg')->path();
 //        HelperBase::dump($res, true);
 
 //        $res = Yii::$app->image->thumb('055239df7c3924.jpg', '200x52', 'resize');
-        $res = Yii::$app->image->thumb('055239df7c3924.jpg', '200x52', 'crop')->path();
+//        $res = Yii::$app->image->thumb('055239df7c3924.jpg', '200x52', 'crop')->path();
 //        echo Html::img($res);
+//        HelperBase::dump($res);
+
+        $res = Yii::$app->image->copy('http://www.hifi4all.dk/ksb/pics/Wisdom%20M50%2011_1_resized2005.jpg', '11223344')->path();
         HelperBase::dump($res);
     }
 }
