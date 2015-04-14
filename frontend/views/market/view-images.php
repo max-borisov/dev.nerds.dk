f0fc840fb2d92653ba0222885d93742f40f7e4b7
max-borisov <max.borisov@yahoo.com>
2015-04-14 15:45:28 +0300

    Update item view page. Fix add info block.

--- frontend/views/market/view-images.php
+++ frontend/views/market/view-images.php
@@ -1,13 +1,13 @@
 <?php
 /* @var $this yii\web\View */
 /* @var $item frontend\models\Item */
+
+$preview = $item->originalPreview($item->preview, '250x150');
 ?>
-<?php if (!empty($item->preview)) { ?>
-    <div class="row img-row margin-bottom">
-        <div class="col-sm-3">
-            <a href="<?= $item->preview ?>" data-toggle="lightbox" data-gallery="global-gallery" data-parent=".img-row" data-footer="<?= $item->description ?>">
-                <img alt="product description" src="<?= $item->preview ?>" data-src="<?= $item->preview ?>" class="img-responsive">
-            </a>
-        </div>
+<div class="row img-row margin-bottom">
+    <div class="col-sm-5">
+        <a href="<?= $item->preview ?>" data-toggle="lightbox" data-gallery="global-gallery" data-parent=".img-row" data-footer="<?= $item->description ?>">
+            <img alt="product description" src="<?= $preview ?>" data-src="<?= $item->preview ?>" class="img-responsive">
+        </a>
     </div>
-<?php } ?>
+</div>
