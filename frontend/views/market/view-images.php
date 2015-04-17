994f769f3fdf413181bb8262ca4c992b6bbf5459
max-borisov <max.borisov@yahoo.com>
2015-04-17 08:51:25 +0300

    Fix image gallery on the item view page.

--- frontend/views/market/view-images.php
+++ frontend/views/market/view-images.php
@@ -6,8 +6,8 @@ $preview = $item->originalPreview($item->preview, '250x150');
 ?>
 <div class="row img-row margin-bottom">
     <div class="col-sm-5">
-        <a href="<?= $item->preview ?>" data-toggle="lightbox" data-gallery="global-gallery" data-parent=".img-row" data-footer="<?= $item->description ?>">
-            <img alt="product description" src="<?= $preview ?>" data-src="<?= $item->preview ?>" class="img-responsive">
+        <a href="<?= $preview ?>" data-toggle="lightbox" data-gallery="global-gallery" data-parent=".img-row" data-footer="">
+            <img alt="product description" src="<?= $preview ?>" data-src="<?= $preview ?>" class="img-responsive">
         </a>
     </div>
 </div>
